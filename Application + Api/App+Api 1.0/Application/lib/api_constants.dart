31ee60ce04f527676ebee681e15ed86050b7b609
kittanai46 <k.kittanai46@gmail.com>
2024-09-11 23:07:13 +0700

    Update api_constants.dart

--- Application + Api/App+Api 1.0/Application/lib/api_constants.dart
+++ Application + Api/App+Api 1.0/Application/lib/api_constants.dart
@@ -1,6 +1,6 @@
 // api_constants.dart
 
-const String baseUrl = 'http://192.168.1.171:4000'; // URL ของเซิร์ฟเวอร์ API
+const String baseUrl = 'http://192.168.120.104:4000'; // URL ของเซิร์ฟเวอร์ API
 
 // ฟังก์ชันสำหรับสร้าง URL เพื่อใช้งาน endpoint การล็อกอิน
 String getLoginEndpoint() {
